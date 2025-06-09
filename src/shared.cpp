5e2723a0ab409b715402fc7852d9d1f9c628aeef
Uranite <62639703+Uranite@users.noreply.github.com>
2025-06-09 18:40:47 +0700

    Implement diffmap, rename distmap -> heatmap
    Disclaimer: This code is AI generated

--- src/shared.cpp
+++ src/shared.cpp
@@ -5,7 +5,7 @@ VapourSynthPluginInit2(VSPlugin* plugin, const VSPLUGINAPI* vspapi) {
     vspapi->configPlugin("com.julek.plugin", "julek", "Julek filters", 4, VAPOURSYNTH_API_VERSION, 0, plugin);
     vspapi->registerFunction("AGM", "clip:vnode;luma_scaling:float:opt;", "clip:vnode;", agmCreate, nullptr, plugin);
     vspapi->registerFunction("AutoGain", "clip:vnode;planes:int[]:opt;", "clip:vnode;", autogainCreate, nullptr, plugin);
-    vspapi->registerFunction("Butteraugli", "reference:vnode;distorted:vnode;distmap:int:opt;intensity_target:float:opt;linput:int:opt;", "clip:vnode;", butteraugliCreate, nullptr, plugin);
+    vspapi->registerFunction("Butteraugli", "reference:vnode;distorted:vnode;distmap:int:opt;heatmap:int:opt;intensity_target:float:opt;linput:int:opt;", "clip:vnode;", butteraugliCreate, nullptr, plugin);
     vspapi->registerFunction("ColorMap", "clip:vnode;type:int:opt;", "clip:vnode;", colormapCreate, nullptr, plugin);
     vspapi->registerFunction("RFS", "clip_a:vnode;clip_b:vnode;frames:int[];mismatch:int:opt;", "clip:vnode;", rfsCreate, nullptr, plugin);
     vspapi->registerFunction("SSIMULACRA", "reference:vnode;distorted:vnode;feature:int:opt;simple:int:opt;", "clip:vnode;", ssimulacraCreate, nullptr, plugin);
